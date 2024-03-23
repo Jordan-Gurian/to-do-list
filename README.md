3a4eb7ce31546b45fcc832b500dc1c152dca09b0
Jordan Gurian <gurianjordan@gmail.com>
2024-03-23 10:52:09 -0700

    Set up files: webpack, index, package, gitignore
    Get development and production set up and install webpack packages

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # to-do-list
+Dynamic to-do list that allows user to add to-do items to specific projects. Will utilize the following tools:
+
+- CSS
+- Javascript
+- Webpack
+- ES6
---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js', 
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+};
