ba1738d73f9ad0ac17479ec2410a80e40e76b277
Jordan Gurian <gurianjordan@gmail.com>
2024-03-24 12:38:58 -0700

    Add delete button to todo
    Button deletes todo element from page and project object

--- src/loadProject.js
+++ src/loadProject.js
@@ -10,23 +10,33 @@ export default function loadProject(project) {
         let newDescription = document.createElement('div');
         let newDue = document.createElement('div');
         let newPriority = document.createElement('div');
+        let deleteButton = document.createElement('button');
 
         newTodo.classList.add('todo');
         newTitle.classList.add('title'); 
         newDescription.classList.add('description'); 
         newDue.classList.add('due'); 
         newPriority.classList.add('priority'); 
+        deleteButton.classList.add('delete-todo');
 
         newTitle.textContent = todo.title;
         newDescription.textContent = todo.description;
         newDue.textContent = todo.due;
         newPriority.textContent = todo.priority;
+        deleteButton.textContent = "Delete";
 
         content.appendChild(newTodo);
         newTodo.appendChild(newTitle);
         newTodo.appendChild(newDescription);
         newTodo.appendChild(newDue);
         newTodo.appendChild(newPriority);
+        newTodo.appendChild(deleteButton);
+
+        deleteButton.addEventListener('click', () => {
+            project.deleteTodo(todo);
+            newTodo.remove();
+        })
+
     }
 
 
